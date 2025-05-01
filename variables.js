5c920ddcc884570f1c8c784ed231d8cf2f273515
Devansh Gupta <dg8878194@gmail.com>
2025-05-01 22:15:07 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,10 @@
+const accountId ="2906"
+var accountEmail ="DEv@123"
+let accountPassword ="123"
+
+accountCity ="Ghaziabad"
+accountEmail ="raghav@123"
+accountPassword ="2509"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword]);
