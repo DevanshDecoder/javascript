4fcb0bafddb39001c99463054044fc7aee9c1788
devansh <DG8878194@gmail.com>
2025-05-01 23:14:26 +0530

    conversion

---
+++ conversion.js
@@ -0,0 +1,8 @@
+let score =20
+console.log(typeof score);
+let mysentence =String(score)
+console.log(typeof mysentence);
+ let value = 45
+ let mystring =Boolean(value);
+ console.log(typeof mystring);
+ console.log(mystring);
