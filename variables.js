ec9d671b436adef3e77a89da19e0f28417716ea6
devansh <DG8878194@gmail.com>
2025-05-03 18:33:04 +0530

    variables

--- variables.js
+++ variables.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const accountId ="2906"
 var accountEmail ="DEv@123"
 let accountPassword ="123"
@@ -8,15 +8,14 @@ accountEmail ="raghav@123"
 accountPassword ="2509"
 
 console.log(accountId);
-=======
-const accountId ="2906"
-var accountEmail ="DEv@123"
-let accountPassword ="123"
+
+// const accountId ="2906"
+// var accountEmail ="DEv@123"
+// let accountPassword ="123"
 
 accountCity ="Ghaziabad"
 accountEmail ="raghav@123"
 accountPassword ="2509"
 
 console.log(accountId);
->>>>>>> 5c920ddcc884570f1c8c784ed231d8cf2f273515
 console.table([accountId,accountEmail,accountPassword]);
