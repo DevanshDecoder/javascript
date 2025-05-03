ac06ae4081602a612e31a22e3caaba835e1f4ad0
devansh <DG8878194@gmail.com>
2025-05-03 18:33:44 +0530

    datatypes

---
+++ data type .js
@@ -0,0 +1,18 @@
+// two types of DATATYPE CAATEGORIES ON THE BASIS OF THEIR STORING VALUES AND ACCESS THEM
+// PRIMITIVE(7):NUMBER,BOOLEAN,STRING,BIGINT,SYMBOL,
+// NON PRIMITIVE(CALL BY REFERENCE)(3):ARRAY,OBJECT,FUNCTION.
+
+const score =100
+console.log(typeof score);
+
+const mySymbol =Symbol("123")
+console.log(mySymbol);
+console.log (typeof mySymbol);
+
+const myString ="Devansh"
+console.log(typeof myString);
+
+// here n will be necessary to write after the number othrwise it will not be a bigInt type of datatype.
+const bigNumber =123456n 
+console.log(typeof bigNumber);
+
